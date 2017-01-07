1a81ffe79b16f54164e77213e3fad09fc5a2025a
Joyce Chau <jyc.chau@gmail.com>
2017-01-07 14:59:13 -0800

    Setup Rails Backend and Frontend Structure

--- BenchBnb/app/controllers/application_controller.rb
+++ BenchBnb/app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.session_token
+  end
+
+  def logout
+    return if current_user.nil?
+    current_user.reset_session_token
+    session[:session_token] = nil
+  end
+
+  def require_signed_in
+    # redirect using json?
+  end
 end
