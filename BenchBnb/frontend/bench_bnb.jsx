b99012fac0558e7501dd910e811e5b6477f6c9cf
Joyce Chau <jyc.chau@gmail.com>
2017-01-09 13:44:10 -0800

    Add SessionReducer, rootReducer, and store

--- BenchBnb/frontend/bench_bnb.jsx
+++ BenchBnb/frontend/bench_bnb.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
+  // window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
+  window.store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnb</h1>, root);
 });
