1447d1302713c48f8479d02dc44308b361723471
Joyce Chau <jyc.chau@gmail.com>
2017-01-09 15:08:36 -0800

    Add React Router and App, Root, and Greeting Components

--- BenchBnb/frontend/bench_bnb.jsx
+++ BenchBnb/frontend/bench_bnb.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
+import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
-  window.store = configureStore();
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnb</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
